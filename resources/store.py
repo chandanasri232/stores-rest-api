d729738594cbe64700c8e483a3e204f3be79531e
chandanasri232 <45327772+chandanasri232@users.noreply.github.com>
2018-12-26 12:06:21 +0530

    Update store.py

--- resources/store.py
+++ resources/store.py
@@ -2,6 +2,18 @@ from flask_restful import Resource, reqparse
 from models.store import StoreModel
 
 class Store(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+    parser.add_argument('store_id',
+        type=int,
+        required=True,
+        help="Every item needs a store id."
+    )
+    
     def get(self, name):
         store = StoreModel.find_by_name(name)
         if store:
